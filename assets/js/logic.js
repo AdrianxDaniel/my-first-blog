218ba6ddfe8b62d5fadcff235b82c104103a3db5
Delta <adriandanieltapia21@gmail.com>
2024-04-02 22:34:12 -0700

    worked on index.html and form/style.css up on to dark and light themes

---
+++ assets/js/logic.js
@@ -0,0 +1,56 @@
+const themeButton = document.querySelector(".toggle");
+
+
+const readingMode = function () {
+  const mode = localStorage.getItem("mode") || "dark";
+
+  return mode;
+};
+
+const readBlogDataFromLocalStorage = function () {
+  const stringData = localStorage.getItem("blogs");
+
+  const data = JSON.parse(stringData);
+
+  return data || [];
+};
+
+const applyMode = function (mode) {
+  let icon, circleColor;
+
+  if (mode === "light") {
+    icon = "♦️";
+    circleColor = "green";
+  } else {
+    icon = "♢";
+    circleColor = "black";
+  }
+  themeButton.textContent = icon;
+  document.body.classList = mode;
+  document.documentElement.style.setProperty("--circle-color", circleColor);
+};
+
+const saveMode = function (mode) {
+  localStorage.setItem("mode", mode);
+};
+
+const handleTheme = function () {
+  const mode = readingMode();
+
+  let newMode;
+
+  if (mode === "light") {
+    newMode = "dark";
+  } else {
+    newMode = "light";
+  }
+
+  applyMode(newMode);
+  alert("newMode")
+
+  saveMode(newMode);
+};
+
+applyMode(readingMode());
+
+themeButton.addEventListener("click", handleTheme);
